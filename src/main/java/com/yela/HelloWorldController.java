596c8ac8d6db568d41e5f351c33e0f9f40c69618
eldar128 <eldar128@gmail.com>
2019-03-15 19:46:13 +0600

    Change to HTML return in controller

--- src/main/java/com/yela/HelloWorldController.java
+++ src/main/java/com/yela/HelloWorldController.java
@@ -3,16 +3,14 @@ package com.yela;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class HelloWorldController {
-    @GetMapping
-    String getView(Model model) {
-        model.addAttribute("msg", "Hello there, This message has been injected from the controller method");
-        return "helloworld";
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String index() {
+        return "index.html";
     }
 }
